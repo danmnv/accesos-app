27dad4c07462e68326f1f12f80d5957cb1bad7c4
Daniel Molina <danmnvx@gmail.com>
2020-06-18 13:42:38 -0500

    Authentication guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+// Auth Guards
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+// Redirects
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['auth/login']);
+const redirectLoggedToDashboard   = () => redirectLoggedInTo(['dashboard/home']);
+
 const routes: Routes = [
   {
     path: 'dashboard',
+    ...canActivate(redirectUnauthorizedToLogin),
     loadChildren: () => import('./dashboard/dash.module').then( m => m.DashboardModule)
   },
   {
     path: 'auth',
+    ...canActivate(redirectLoggedToDashboard),
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
   },
   {
