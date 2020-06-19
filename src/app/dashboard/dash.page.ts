aac6bd3166ad58b2ff2a502b9005b5f87d0aef4b
Daniel Molina <danmnvx@gmail.com>
2020-06-18 19:58:44 -0500

    Opciones de botón y pestaña de account

--- src/app/dashboard/dash.page.ts
+++ src/app/dashboard/dash.page.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonTabs } from '@ionic/angular';
+import { IonTabs, ModalController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AccountComponent } from '../components/account/account.component';
 
 @Component({
   selector: 'app-home',
@@ -10,17 +12,40 @@ export class Dashboard {
 
   @ViewChild('menuTabs') tabs: IonTabs;
 
-  title: String = 'Home';
+  title: string = 'home';
+  options: String = 'add-circle-outline'
   items: Array<{ path: string, title: string, icon: string}> = [
     { path: 'home', title: 'Home', icon: 'home-sharp' },
     { path: 'record', title: 'Record', icon: 'server' },
-    { path: 'account', title: 'Account', icon: 'person-circle' },
+    // { path: 'account', title: 'Account', icon: 'person-circle' },
   ];
 
-  constructor() {}
+  constructor(private afAuth: AngularFireAuth, public modalController: ModalController) { }
 
   tabChanged() {
     this.title = this.tabs.getSelected();
+    switch (this.title) {
+      case 'home':
+        this.options = 'add-circle-outline';
+        break;
+      case 'record':
+        this.options = 'filter-outline';
+        break;
+      // case 'account':
+      //   this.options = 'settings-outline';
+      // break;
+      default:
+        this.options = 'ellipsis-vertical'
+        break;
+    }
+  }
+
+  async openAccount() {
+    const modal = await this.modalController.create({
+      component: AccountComponent
+    });
+
+    return await modal.present();
   }
 
 }
