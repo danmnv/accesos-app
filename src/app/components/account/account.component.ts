a28cfc681347d438a974f811940fb6e8651df552
Daniel Molina <danmnvx@gmail.com>
2020-06-18 19:58:57 -0500

    Vista de componente account y log out

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, AlertController } from '@ionic/angular';
+import { temporaryDeclaration } from '@angular/compiler/src/compiler_util/expression_converter';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account',
@@ -7,8 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modalController: ModalController, public alertController: AlertController, private afAuth: AngularFireAuth, private router: Router) { }
 
   ngOnInit() {}
 
+  // Delete session
+  logOut() {
+    this.afAuth.signOut().then(() => {
+      this.dismissModal();
+      this.router.navigate(['/auth/login']);
+    });
+  }
+
+  // Alert to logout
+  async alertConfirmation() {
+    const alert = await this.alertController.create({
+      header: 'Log out',
+      message: 'Are you sure?',
+      buttons: [
+        { text: 'Cancel', role: 'cancel' }, 
+        { text: 'Ok', handler: () => this.logOut() }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  // Close modal
+  async dismissModal() {
+    await this.modalController.dismiss();
+  }
 }
