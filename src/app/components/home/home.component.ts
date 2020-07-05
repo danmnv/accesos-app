e72128861e010b131d1e3e37cdba5d460e95c054
Daniel Molina <danmnvx@gmail.com>
2020-07-05 02:23:53 -0500

    Preview scanner

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Subscription, interval } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 import CryptoJS from 'crypto-js';
+import jsQR from 'jsqr';
 
 import { environment } from '../../../environments/environment';
+import { ToastController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -18,18 +20,29 @@ export class HomeComponent implements OnInit, OnDestroy {
   coded: String = '';
   subscription : Subscription;
   timer: Observable<number> = interval(100);
-  loadData: Promise<boolean>;
   
   showQR: Boolean = false;
+  scanActive: Boolean = false;
+  videoElement: any;
+  canvasElement: any;
+  canvasContext: any;
 
-  constructor(private user$: UserService) {}
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  
+  loading: HTMLIonLoadingElement;
 
+  constructor(private user$: UserService, private toastCtrl: ToastController, private loadingCtrl: LoadingController) {}
+
+  ngAfterViewInit(): void {
+    this.videoElement = this.video.nativeElement;
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvas.nativeElement.getContext('2d');
+  }
   ngOnInit() {
     this.user$.afAuth.onAuthStateChanged(auth => {
       if (auth) {
         this.user$.user.subscribe(user => {
-          // this.user$.getLocalUser().then(user => {
-            console.log(user);
             if (user.admin) {
               this.showQR = false;
             }
@@ -37,10 +50,8 @@ export class HomeComponent implements OnInit, OnDestroy {
               this.showQR = true;
               this.autogenCode(user.id);
               this.subscription = this.timer.subscribe(() => this.setTimerValue(user.id));
-              this.loadData = Promise.resolve(true);
             }
           })
-        // })
       }
     })
   }
@@ -58,6 +69,62 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   autogenCode(uid: string) {
     this.coded = CryptoJS.AES.encrypt(uid, environment.secret_key).toString();
+  }
+
+  async startScan() {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'enviroment'} });
+    this.videoElement.srcObject = stream;
+    this.videoElement.setAttribute('playsinline', true);
+    this.videoElement.play();
+
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+
+      this.canvasContext.drawImage(this.videoElement, 0, 0, this.canvasElement.height, this.canvasElement.width);
+
+      const imageData = this.canvasContext.getImageData(0, 0, this.canvasElement.height, this.canvasElement.width);
+
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'attemptBoth'
+      })
+      console.log(code);
+
+      if (code) {
+        this.showConfirm(`Token_ ${CryptoJS.AES.decrypt(code.data, environment.secret_key).toString(CryptoJS.enc.Utf8)}`);
+        this.scanActive = false;
+      }
+      else if (this.scanActive) requestAnimationFrame(this.scan.bind(this));
+
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+  action() {
+    this.scanActive = !this.scanActive;
+    
+    if (this.scanActive) this.startScan();
+  }
+
+  async showConfirm(decrypt) {
+    const toast = await this.toastCtrl.create({
+      message: decrypt
+    })
+
+    toast.present();
   }
 
 }
