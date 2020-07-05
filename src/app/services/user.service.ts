3154f94dcd99ed3639bdd29aa5bee7112f11ca83
Daniel Molina <danmnvx@gmail.com>
2020-07-05 00:10:29 -0500

    Preview de usuario guardado localmente

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument, Action, DocumentSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Storage } from '@ionic/storage';
+import { map } from 'rxjs/operators';
 
 interface User {
+  id: string,
   email: String,
   username: String,
   name: String,
   ap_pat: String,
   ap_mat?: String,
-  rol: Boolean
+  admin: Boolean
 }
 
 @Injectable({
@@ -21,23 +23,34 @@ class UserService {
   private itemDoc: AngularFirestoreDocument<User>;
   user: Observable<User>;
 
-  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore, private storage: Storage) {
+  constructor(public afAuth: AngularFireAuth, private afs: AngularFirestore, private storage: Storage) {
     // Get user
     this.afAuth.onAuthStateChanged(user => {
       if (user) this.setUser(user.uid);
     });
   }
 
+  documentToObject = (ref: Action<DocumentSnapshot<User>>) => {
+    const object = ref.payload.data();
+    object.id = ref.payload.id;
+
+    return object;
+  }
+
   // Set document user
   setUser(uid: String) {
     this.itemDoc = this.afs.doc<User>(`users/${uid}`);
-    this.user = this.itemDoc.valueChanges();
-    this.user.subscribe(user => this.storage.set('user', { docid: uid, ...user }));
+    this.user = this.itemDoc.snapshotChanges().pipe(map(reference => this.documentToObject(reference)));
+    // this.user.subscribe(user => this.storage.set('user', { docid: uid, ...user }));
   }
 
-  getLocalUser(): Promise<any> {
-    return this.storage.get('user');
-  }
+  // async getLocalUser(): Promise<any> {
+  //   return await this.storage.get('user');
+  // }
+
+  // async destroyLocalUser(): Promise<any> {
+  //   return await this.storage.remove('user');
+  // }
 }
 
 export { User, UserService };
