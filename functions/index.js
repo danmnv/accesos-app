0b39d19092510e8b65119fb10cbbf9e09a962eed
Daniel Molina <danmnvx@gmail.com>
2020-07-05 20:50:29 -0500

    validateToken function in Firebase

--- functions/index.js
+++ functions/index.js
@@ -9,3 +9,21 @@ exports.createToken = functions.https.onCall((data, context) => {
 
     return { ok: true, jwt: token };
 });
+
+exports.validateToken = functions.https.onCall((data, context) => {
+    try {
+        // Obtención de token por encabezado Bearer.
+        let token = data.token;
+        // Decodificar token
+        let decoded = jwt.verify(token, process.env.PRIVATE_KEY, { ignoreExpiration: false });
+
+        // Permitir acceso
+        if (decoded) {
+            return { ok: true, msg: "Token válido" };
+        }
+    }
+    catch(error) {
+        // Token inválido
+        return { ok: false, unauth: true, msg: "Token caducado", err: error };
+    }
+});
