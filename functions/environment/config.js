695892365b3eb1afa3e333b6a935643de9beb81c
Daniel Molina <danmnvx@gmail.com>
2020-07-05 21:38:44 -0500

    Token expires

--- functions/environment/config.js
+++ functions/environment/config.js
@@ -2,4 +2,4 @@
 process.env.PRIVATE_KEY = process.env.PRIVATE_KEY || 't2vjd@Nzh#Bo9mT^8S3@';
 
 // JWT expiration
-process.env.EXPIRATION = process.env.EXPIRATION || '1m';
+process.env.EXPIRATION = process.env.EXPIRATION || '5m';
