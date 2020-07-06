dd8877134c2e1f61e83622c808523e2a3f872e83
Daniel Molina <danmnvx@gmail.com>
2020-07-05 21:40:33 -0500

    Allow access from validateToken

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,13 @@
 require('./environment/config');
 
 const functions = require('firebase-functions');
+const fireabse = require ('firebase-admin');
 const jwt = require('jsonwebtoken');
+
+fireabse.initializeApp();
+
+const firestore = fireabse.firestore();
+const collection = firestore.collection('users');
 
 exports.createToken = functions.https.onCall((data, context) => {
     let token = jwt.sign({ uid: context.auth.uid }, process.env.PRIVATE_KEY, { expiresIn: process.env.EXPIRATION });
@@ -10,20 +16,28 @@ exports.createToken = functions.https.onCall((data, context) => {
     return { ok: true, jwt: token };
 });
 
-exports.validateToken = functions.https.onCall((data, context) => {
+exports.validateToken = functions.https.onCall(async (data, context) => {
     try {
-        // Obtención de token por encabezado Bearer.
+        // Token
         let token = data.token;
-        // Decodificar token
+        // Decode token
         let decoded = jwt.verify(token, process.env.PRIVATE_KEY, { ignoreExpiration: false });
 
-        // Permitir acceso
+        // Decoded token
         if (decoded) {
-            return { ok: true, msg: "Token válido" };
+            // Get user doc from firestore
+            let { exists, user } = await collection.doc(decoded.uid).get().then(doc => {
+                return { exists: doc.exists, user: doc.data() }
+            });
+
+            // If exist allow access
+            if (exists) return { ok: true, msg: `Acceso permitido: ${user.name} ${user.ap_pat}` };
+            // Don't allow
+            else return { ok: false, msg: "Usuario inexistente" };
         }
     }
     catch(error) {
-        // Token inválido
+        // Invalid token
         return { ok: false, unauth: true, msg: "Token caducado", err: error };
     }
 });
